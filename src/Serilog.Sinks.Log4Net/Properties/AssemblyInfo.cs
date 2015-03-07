1a3bc69f555e0d8e9b57c6995bf84e5bf551a5b2
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2015-03-07 15:08:03 +1000

    Moved source from serilog/serilog (no build yet)

---
+++ src/Serilog.Sinks.Log4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Serilog.Sinks.Log4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
