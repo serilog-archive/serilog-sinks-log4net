ccd98854df0e98a733bf7c28fa25f455db9521a5
mparge <mathias.parge@gmx.de>
2019-12-22 11:59:11 +0100

    Changed project to multi target (netstandard2.0;net45). Updated references to current versions. Adjusted LogManager.GetLogger.

--- src/Serilog.Sinks.Log4Net/Sinks/Log4Net/Log4NetSink.cs
+++ src/Serilog.Sinks.Log4Net/Sinks/Log4Net/Log4NetSink.cs
@@ -13,11 +13,11 @@
 // limitations under the License.
 
 using System;
-using System.Security.AccessControl;
 using Serilog.Core;
 using Serilog.Debugging;
 using Serilog.Events;
 using log4net;
+using System.Reflection;
 
 namespace Serilog.Sinks.Log4Net
 {
@@ -55,8 +55,8 @@ namespace Serilog.Sinks.Log4Net
 
             var message = logEvent.RenderMessage(_formatProvider);
             var exception = logEvent.Exception;
-
-            var logger = LogManager.GetLogger(loggerName);
+            
+            var logger = LogManager.GetLogger(Assembly.GetCallingAssembly(), loggerName);
             
             using (_supplyContextMessage ? ThreadContext.Stacks["NDC"].Push(ContextMessage) : new NullDisposable())
             {
